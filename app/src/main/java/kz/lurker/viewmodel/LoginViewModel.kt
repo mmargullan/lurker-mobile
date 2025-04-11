2ea94f2a8cfcbb7a752eb1f1775d1b8b970a1dd7
Margulan <m.begzhan@aster.kz>
2025-04-11 11:40:26 +0500

    activity logic separated into viewmodel and ui for structures sake

---
+++ app/src/main/java/kz/lurker/viewmodel/LoginViewModel.kt
@@ -0,0 +1,69 @@
+package kz.lurker.viewmodel
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.*
+import io.ktor.client.*
+import io.ktor.client.call.body
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kz.lurker.service.TokenService
+
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val tokenService = TokenService(application)
+
+    private val client = HttpClient(OkHttp) {
+        install(ContentNegotiation) {
+            json(Json { ignoreUnknownKeys = true })
+        }
+    }
+
+    private val _loginResult = MutableLiveData<String>()
+    val loginResult: LiveData<String> = _loginResult
+
+    fun login(username: String, password: String) {
+        viewModelScope.launch {
+            try {
+                val response = client.post("http://10.0.2.2:8081/user/login") {
+                    contentType(ContentType.Application.Json)
+                    setBody(LoginRequest(username, password))
+                }
+
+                if (response.status == HttpStatusCode.OK) {
+                    val responseBody = response.body<LoginResponse>()
+                    tokenService.saveToken(responseBody.token)
+                    _loginResult.value = "Login successful"
+                } else {
+                    _loginResult.value = "Login failed: ${response.status}"
+                }
+            } catch (e: Exception) {
+                _loginResult.value = "Error: ${e.message}"
+            }
+        }
+    }
+
+    @Serializable
+    data class LoginRequest(val login: String, val password: String)
+
+    @Serializable
+    data class LoginResponse(val message: String, val status: String, val token: String)
+
+}
+
+class LoginViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            LoginViewModel(application) as T
+        } else {
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+}
